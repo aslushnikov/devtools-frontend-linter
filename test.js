889172c67e5806131e0b53de3279d8a96582c78f
Andrey Lushnikov <aslushnikov@gmail.com>
2013-06-09 22:14:12 +0400

    tests/test.js -> test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-  , analyzer = require('../ast-analyzer.js')
+  , analyzer = require('./ast-analyzer.js')
 
 console.assert(process.argv.length > 2);
 var files = process.argv.slice(2);
@@ -7,6 +7,7 @@ var files = process.argv.slice(2);
 for(var i = 0; i < files.length; ++i) {
     var data = fs.readFileSync(files[i], "utf-8");
     var ast = analyzer.parseAST(data);
+    console.log("=====" + files[i] + "=====");
     dumpUsedClasses(ast);
     dumpDefinedFunctions(ast);
     dumpClassPrototypes(ast);
@@ -39,6 +40,6 @@ function dumpClassPrototypes(ast) {
         var properties = [];
         for(var j in proto.props)
             properties.push(j);
-        console.log(i + ": super=" + protos[i].superClass + " props=" + properties.join(", "));
+        console.log(i + ": super=" + protos[i].superClass + " props={" + properties.join(",") + "}");
     }
 }
