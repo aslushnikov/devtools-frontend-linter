c93d38f7918b9cd6751bf8c5879cfb3cd023a6b6
Andrey Lushnikov <aslushnikov@gmail.com>
2013-06-10 03:58:43 +0400

    Making main.js better

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+#!/usr/bin/env node
+
 var Project = require('./project.js')
   , devTools = require('./devtools-analyzer.js')
   , renderer = require('./devtools-report-renderer.js')
 
 console.assert(process.argv.length > 2);
 var proj = Project.createFromFiles(process.argv.slice(2));
-var report = devTools.checkClassInheritance(proj);
-var rendered = renderer.renderReport(report);
-for(var i = 0; i < rendered.length; ++i)
-    console.log(rendered[i]);
+var report = renderer.renderReport(devTools.checkClassInheritance(proj));
+if (report.length) {
+    for(var i = 0; i < report.length; ++i)
+        console.log(report[i]);
+} else {
+    console.log("0 errors found");
+}
